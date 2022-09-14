8f5b2db54e4cc3155f46240bfbcaf355cd804a6b
zzzxiaohong <hongxiaozhang@bjtu.edu.cn>
2022-09-14 18:33:27 +0800

    Update scripts

--- scripts/lexical_induction/run_map.sh
+++ scripts/lexical_induction/run_map.sh
@@ -1,13 +1,12 @@
 work_dir= # your working directory path 
-
 embed_path=$work_dir/output/embed
 out_path=$work_dir/output/dict/din
 code_dir=$work_dir/tools/vecmap
 mkdir -p $out_path
 sl=en
 tl=zh
-
-CUDA_VISIBLE_DEVICES=3,4 python $code_dir/map_embeddings.py \
+gpu_ids=$1
+CUDA_VISIBLE_DEVICES=gpu_ids python $code_dir/map_embeddings.py \
     --unsupervised \
     --cuda \
     $embed_path/all-train.mono.$sl.vec \
